efb2b78d4a11c216668d52ecbc55c54aad11a96a
Lay Flags <code@layfla.gs>
2016-10-13 17:59:29 +0200

    apply patch to relative-time-webcomponent to fix bug

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = require('relative-time-webcomponent')
+var component = require('relative-time-webcomponent');
+
+// apply patch to fix: setting `autoupdate` doesn't call `tick`
+var origMethod = component.prototype.attributeChangedCallback;
+component.prototype.attributeChangedCallback = function (attr, oldVal, newVal) {
+  if (attr === 'autoupdate') attr = 'datetime'
+  origMethod.call(this, attr, oldVal, newVal)
+}
+
+module.exports = component
